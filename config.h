7b5e58921821cc02224f96859ab33a945dc37bf8
Andre Vallestero <andrevallestero@gmail.com>
2019-02-12 17:35:07 -0500

    Updated cursor to be negative

--- config.h
+++ config.h
@@ -119,8 +119,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
